dabf808ec006600fb2eef75abeed7ebba572ab98
Lord Rennan <imael.rennan@ufms.br>
2023-03-30 11:26:37 -0400

    estilização de index.jsx

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -2,7 +2,7 @@ import './styles.css';
 export function Home() { 
 
   return (
-  <div>
+  <div className='container'>
     <h1>Lista de Presença</h1>
     <input type="text" placeholder="Digite o nome..."/>
     <button type="button">Adicionar</button>
